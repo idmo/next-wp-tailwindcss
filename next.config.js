40980b20a13c2df5d5d4248fbd43f91bf23a7937
Brian Maggi <brianmaggi@gmail.com>
2021-11-02 18:49:06 -0700

    fixed config, took out social image

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@ const withPlugins = require('next-compose-plugins');
 
 const indexSearch = require('./plugins/search-index');
 const feed = require('./plugins/feed');
-const socialImages = require('./plugins/socialImages');
+// const socialImages = require('./plugins/socialImages');
 // const sitemap = require('./plugins/sitemap');
 
 // module.exports = withPlugins([[indexSearch], [feed], [sitemap], [socialImages]], {
 // took out ^sitemap for now.
 
-module.exports = withPlugins([[indexSearch], [feed], [socialImages]], {
+module.exports = withPlugins([[indexSearch], [feed]], {
   // By default, Next.js removes the trailing slash. One reason this would be good
   // to include is by default, the `path` property of the router for the homepage
   // is `/` and by using that, would instantly create a redirect
